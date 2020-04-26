d00a8dd791fc3bdc84731aeaf71c90ddf6d8fdf4
Jonathan van Westendorp <mrjonathanvw@gmail.com>
2020-04-26 22:26:19 +0000

    Fix relativity

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ if [ "$(docker ps -q -f name=tx-tool)" ]; then
 fi
 
 TOOLPATH=$( dirname "$PWD" )
-
+echo $TOOLPATH
 echo "Building web tool..." 
-docker build -t tx-tool . && docker run --name=tx-tool -d --rm -v $TOOLPATH/src:/var/www/html -p 80:80 tx-tool
+docker build -t tx-tool $TOOLPATH && docker run --name=tx-tool -d -v $TOOLPATH/src:/var/www/html -p 80:80 tx-tool
 echo "Done!"
