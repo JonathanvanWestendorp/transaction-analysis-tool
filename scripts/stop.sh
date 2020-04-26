eeb56b36b273e529adfe9e875ea5bf25efe6e0b3
Jonathan van Westendorp <mrjonathanvw@gmail.com>
2020-04-26 22:18:36 +0000

    cleanup

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Removing leftover instances and images"
-docker stop tx-tool &&  docker rm tx-tool && docker rmi tx-tool
+docker stop tx-tool && docker rm tx-tool && docker rmi tx-tool
 echo "Done!"
