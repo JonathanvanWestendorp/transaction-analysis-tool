d37d1ed191471d7f18a1006b1d5c8ee2095ba800
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-27 16:23:11 +0200

    finish loading file functionality

--- src/contract.js
+++ src/contract.js
@@ -1,9 +1,12 @@
-//var solc = require('solc');
+var solc = require('solc');
 
 function parseContract() {
     const contract = document.getElementById("contract");
     if (contract) {
-        alert("File has arrived successfully");
-        console.log(contract);
+        fileReader = new FileReader();
+        fileReader.readAsText(contract.files[0]);
+        fileReader.onload = function() {
+            console.log(read.result);
+        }
     }   
 }
