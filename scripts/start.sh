4284743fc1c1973e6c721382ff974b472878ec04
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 00:53:22 +0200

    Small changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# For volume mapping use: -v $TOOLPATH/src:/var/www/html
+
 echo "Checking for running tool..."
 if [ "$(docker ps -aq -f name=tx-tool)" ]; then
     /bin/bash ./stop.sh
@@ -8,5 +10,5 @@ fi
 TOOLPATH=$( dirname "$PWD" )
 echo $TOOLPATH
 echo "Building web tool..." 
-docker build -t tx-tool $TOOLPATH && docker run --name=tx-tool -d -v $TOOLPATH/src:/var/www/html -p 80:80 tx-tool
+docker build -t tx-tool $TOOLPATH && docker run --name=tx-tool -d -p 80:80 tx-tool
 echo "Done!"
