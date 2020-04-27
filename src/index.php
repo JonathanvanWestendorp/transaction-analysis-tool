6fd18b302a2bd68973e1ca76ba3409d98bdb51fe
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-27 11:07:04 +0200

    Add index.php && git push

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html style="--theme:dark;">
+    <title>
+      Transaction Analysis
+    </title>
+    <body>
+      <div>
+        <div>
+          Deployed Contract
+        </div>
+        <div>
+          <?php
+            echo "This Works!";
+          ?>
+        </div>
+      </div>
+    </body>
+  </html>
