68b5f44a1d2662f60dc884b6196356d2a8b124c1
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 00:57:52 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // var solc = require('solc');
 const express = require('express');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.multipart());
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.multipart());
 
 app.post('/compile', function (req, res) {
     var body = req.body;
