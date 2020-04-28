4e48ef6f2f5a61da2c01e045719f6ad2dcfdb47f
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 21:51:56 +0200

    Small changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// var solc = require('solc');
+// const solc = require('solc');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 // app.use(express.json());
@@ -6,9 +7,9 @@ const app = express();
 // app.use(express.multipart());
 
 app.post('/compile', function (req, res) {
-    console.log("Incoming request:");
+    console.log(req);
     // compile with solidity
-    res.send("Hij is binnen hoor");
+    res.status(200).send({ontvangen: true});
 });
 
 app.listen(3000);
