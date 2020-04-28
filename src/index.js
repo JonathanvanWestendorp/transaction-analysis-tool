695cad38c8985905afaacf2b1244252b35be08c4
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 23:35:13 +0200

    Small changes

--- src/index.js
+++ src/index.js
@@ -8,13 +8,10 @@ app.use(busboy());
 app.post('/compile', function (req, res) {
     req.pipe(req.busboy);
     req.busboy.on('file', function (_, file, filename) {
-        // console.log(file);
-        // data = fs.readFile(file, "utf8", function (err, data) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        //     return data;
-        // });
+        var saveTo = path.join(__dirname, 'uploads/' + filename);
+        file.pipe(fs.createWriteStream(saveTo));
+
+
         // var input = {
         //     language: 'Solidity',
         //     sources: {
@@ -32,8 +29,6 @@ app.post('/compile', function (req, res) {
         // };
         // var output = JSON.parse(solc.compile(JSON.stringify(input)));
         // console.log(output);
-        var output = solc.compile(file);
-        console.log(output)
     });
     res.status(200).send({ontvangen: true});
 });
