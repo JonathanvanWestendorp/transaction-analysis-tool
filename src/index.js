f54520bf8edeedf99daa2df7f20e42508e40b6f6
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 23:57:22 +0200

    Small changes

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ app.post('/compile', function (req, res) {
                 }
             }
         };
-        var output = JSON.parse(solc.compile(JSON.stringify(input)));
-        console.log(output);
+        // var output = JSON.parse(solc.compile(JSON.stringify(input)));
+        // console.log(output);
     });
     res.status(200).send({ontvangen: true});
 });
