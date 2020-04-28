a2b853d0d6733ca80ad7732999fd06be41b50288
Jonathan van Westendorp <jonathan@supportdesk.nu>
2020-04-28 22:10:23 +0200

    Small changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-app.use(bodyParser())
+app.use(bodyParser.json());
+app.use(bodyParser.multipart());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.post('/compile', function (req, res) {
     console.log(req.body);
